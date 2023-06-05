ecb571655b02db7da4c644a15bfb2f39cf7c50fc
Igor <igoryan>
2023-06-06 00:15:31 +0300

    no message

---
+++ Repositories/MemoryProvider.cs
@@ -0,0 +1,16 @@
+﻿using TspuWebLabs.Data;
+
+namespace igoryan.Repositories
+{
+    public class MemoryProvider
+    {
+        public List<User> Users { get; set; }
+        public int ValidId { get; set; }
+        public MemoryProvider()
+        {
+            Users = new List<User>();
+            ValidId = 0;
+        }
+
+    }
+}
